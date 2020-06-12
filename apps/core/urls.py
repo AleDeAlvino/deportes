74a450ab8b5f11ef6ba73f138238fbc99a8a1887
Ale De Alvino <alejandra.dealvino.melgoza@gmail.com>
2020-06-12 14:48:16 -0500

    Implementar auth

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -3,6 +3,8 @@ from .import views
 
 urlpatterns = [
     path('', views.home_view, name="home"),
+    path('login', views.login_view, name="login"),
+    path('logout', views.logout_view, name="logout"),
     path('profile/', views.profile_view, name="profile"),
     path('changePassword/', views.changePassword_view, name="changePassword"),
 ]
